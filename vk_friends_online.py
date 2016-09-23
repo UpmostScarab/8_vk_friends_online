7edc0ce915038973947c952f814c89db87ca4166
aumnov <sasha@umnov.org>
2016-09-23 18:54:55 +0300

    первая итерация

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,16 @@
 import vk
+import getpass
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID =  '5218358'  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 
 def get_user_login():
-    pass
+    return input('Логин: ')
 
 
 def get_user_password():
-    pass
+    return getpass.getpass('Пароль: ')
 
 
 def get_online_friends(login, password):
@@ -19,11 +20,13 @@ def get_online_friends(login, password):
         user_password=password,
     )
     api = vk.API(session)
+    return api.friends.get(fields=['online'])
     # например, api.friends.get()
 
 
 def output_friends_to_console(friends_online):
-    pass
+    for friend in friends_online:
+        print('%s %s' % (friend['first_name'],friend['last_name']))
 
 if __name__ == '__main__':
     login = get_user_login()
